f50b36a7d9634e506683d36dbf69bb3ba8f2c4dd
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-11 16:10:44 -0400

    added methods for api

--- lib/api.ts
+++ lib/api.ts
@@ -30,3 +30,63 @@ export async function fetchPost(id: string) {
     const data = await res.json();
     return data;
 }
+export async function fetchTags() {
+    const res = await fetch(`${API_URL}/tags`);
+    if (!res.ok) {
+        throw new Error(`Error fetching tags: ${res.statusText}`);
+    }
+    return res.json();
+}
+
+export async function fetchTagTypes() {
+    const res = await fetch(`${API_URL}/tags/tag_types`);
+    if (!res.ok) {
+        throw new Error(`Error fetching tag types: ${res.statusText}`);
+    }
+    return res.json();
+}
+
+export async function createTag(name: string, tagType: string) {
+    const res = await fetch(`${API_URL}/tags`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ tag: { name, tag_type: tagType } }),
+    });
+    if (!res.ok) {
+        throw new Error(`Error creating tag: ${res.statusText}`);
+    }
+    return res.json();
+}
+export async function createTagType(name: string) {
+    const res = await fetch(`${API_URL}/tags/tag_types`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ tag_type: { name } }),
+    });
+    if (!res.ok) {
+        throw new Error(`Error creating tag type: ${res.statusText}`);
+    }
+    return res.json();
+}
+
+export async function deleteTag(id: number) {
+    const res = await fetch(`${API_URL}/tags/${id}`, {
+        method: 'DELETE',
+    });
+    if (!res.ok) {
+        throw new Error(`Error deleting tag: ${res.statusText}`);
+    }
+}
+
+export async function deleteTagType(id: number) {
+    const res = await fetch(`${API_URL}/tags/tag_types/${id}`, {
+        method: 'DELETE',
+    });
+    if (!res.ok) {
+        throw new Error(`Error deleting tag type: ${res.statusText}`);
+    }
+}
