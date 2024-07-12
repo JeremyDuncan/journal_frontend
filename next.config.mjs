465520da661b224d7ddde67cc00e9b5b03ea5462
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-12 13:23:49 -0400

    added comments

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// USE TO DISABLE STRICT MODE (PREVENTS DOUBLE QUERY OF API)
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: process.env.NODE_ENV === 'production',
+// };
+//
+// export default nextConfig;
