099823a809fb2b8cfdbee7f29463cfeabcef8aaf
JeremyDuncan <jeremy.duncan1984@gmail.com>
2024-07-19 16:21:38 +0000

    updated modal styling

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -25,6 +25,8 @@ const CalendarView: React.FC = () => {
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
     const modalRef = useRef<HTMLDivElement>(null);
     const closeButtonRef = useRef<HTMLButtonElement>(null);
+    const filterModalRef = useRef<HTMLDivElement>(null);
+    const filterCloseButtonRef = useRef<HTMLButtonElement>(null);
     const [loadingPosts, setLoadingPosts] = useState(true);
     const [collapsedTags, setCollapsedTags] = useState<Record<string, boolean>>({});
 
@@ -156,16 +158,6 @@ const CalendarView: React.FC = () => {
         setIsFilterModalOpen(false);
     };
 
-    useEffect(() => {
-        if (isModalOpen && modalRef.current && closeButtonRef.current) {
-            const modalHeight = modalRef.current.offsetHeight;
-            const modalTop = modalRef.current.getBoundingClientRect().top;
-            const buttonTop = modalTop + modalHeight + 20; // 20px below the modal
-
-            closeButtonRef.current.style.top = `${buttonTop}px`;
-        }
-    }, [isModalOpen, selectedDatePosts]);
-
     const getFormattedDate = (date: Date | null) => {
         if (!date) return '';
         return format(date, 'EEEE - MMMM do, yyyy');
@@ -215,85 +207,80 @@ const CalendarView: React.FC = () => {
 
             {isFilterModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 overflow-y-auto">
-                    <div className="relative">
-                        <div ref={modalRef} className="modal-container bg-gray-300 text-black p-6 rounded-lg shadow-lg">
-                            <h2 className="text-2xl font-bold mb-4 text-center">Filter by Tags</h2>
-                            <div className="mb-4 p-4 border rounded bg-gray-800 text-white relative">
-                                <div className="block text-white text-sm font-bold mb-2">
-                                    <h1>TAGS</h1>
-                                    <FiInfo
-                                        data-tooltip-id={`tagTooltip`}
-                                        data-tooltip-content={`Select Tags to filter by on the calendar`}
-                                        className="absolute top-2 right-2 text-white"
-                                    />
-                                    <Tooltip id={`tagTooltip`} />
+                    <div ref={filterModalRef} className="flex-modal-container bg-gray-300 text-black rounded-lg shadow-lg">
+                        <h2 className="text-2xl font-bold mb-2 text-center">Filter by Tags</h2>
+                            <FiInfo
+                                data-tooltip-id={`tagTooltip`}
+                                data-tooltip-content={`Select Tags to filter by on the calendar`}
+                                className="absolute  text-black"
+                            />
+                            <Tooltip id={`tagTooltip`} />
+                        <div className="modal-content mb-0 p-4 border rounded bg-gray-800 text-white relative">
+                            {selectedTags.length > 0 && (
+                                <div className="mb-4 p-4 border rounded bg-gray-700 text-white relative">
+                                    <h2 className="text-xl font-bold mb-2">Selected Tags</h2>
+                                    <div className="flex flex-wrap gap-2">
+                                        {selectedTags.map((tag) => (
+                                            <div className="flex gap-0">
+                                                <div key={tag.id} className="flex items-center bg-gray-600 px-2 py-1 rounded-l" style={{ backgroundColor: tag.tag_type.color }}>
+                                                    <span>{tag.name}</span>
+                                                </div>
+                                                <div className="bg-gray-300 hover:bg-gray-800 cursor-pointer pl-0 pt-2 pr-2 rounded-r"
+                                                     onClick={() => handleRemoveTag(tag)}
+                                                >
+                                                    <AiOutlineClose
+                                                        className="ml-2 text-red-500"
+                                                    />
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                                {selectedTags.length > 0 && (
-                                    <div className="mb-4 p-4 border rounded bg-gray-700 text-white relative">
-                                        <h2 className="text-xl font-bold mb-2">Selected Tags</h2>
+                            )}
+                            {Object.entries(groupedTags).map(([tagType, tags]) => (
+                                <div key={tagType} className="mb-2 p-4 pt-2 rounded bg-gray-700 text-white relative">
+                                    <div
+                                        className="flex items-center justify-between mb-2 border-b border-gray-600 cursor-pointer"
+                                        onClick={() => handleToggleCollapse(tagType)}
+                                    >
+                                        <span
+                                            className="mt-0 p-0"
+                                            style={{ textShadow: `2px 2px 4px ${tags[0].tag_type.color}`, color: 'white' }}
+                                        >
+                                            {tagType}
+                                        </span>
+                                        <span>{collapsedTags[tagType] ? '▼' : '▲'}</span>
+                                    </div>
+                                    {!collapsedTags[tagType] && (
                                         <div className="flex flex-wrap gap-2">
-                                            {selectedTags.map((tag) => (
-                                                <div className="flex gap-0">
-                                                    <div key={tag.id} className="flex items-center bg-gray-600 px-2 py-1 rounded-l" style={{ backgroundColor: tag.tag_type.color }}>
-                                                        <span>{tag.name}</span>
-                                                    </div>
-                                                    <div className="bg-gray-300 hover:bg-gray-800 cursor-pointer pl-0 pt-2 pr-2 rounded-r"
-                                                         onClick={() => handleRemoveTag(tag)}
+                                            {tags.map((tag) => (
+                                                <div key={tag.id} className="flex items-center">
+                                                    <input
+                                                        type="checkbox"
+                                                        id={tag.name}
+                                                        value={tag.name}
+                                                        checked={selectedTags.some((t) => t.id === tag.id)}
+                                                        onChange={() => handleTagSelection(tag)}
+                                                        className="mr-2"
+                                                    />
+                                                    <label
+                                                        htmlFor={tag.name}
+                                                        className="px-2 py-1 rounded"
+                                                        style={{ backgroundColor: tag.tag_type.color, color: 'white' }}
                                                     >
-                                                        <AiOutlineClose
-                                                            className="ml-2 text-red-500"
-                                                        />
-                                                    </div>
+                                                        {tag.name}
+                                                    </label>
                                                 </div>
                                             ))}
                                         </div>
-                                    </div>
-                                )}
-                                {Object.entries(groupedTags).map(([tagType, tags]) => (
-                                    <div key={tagType} className="mb-2 p-4 pt-2 rounded bg-gray-700 text-white relative">
-                                        <div
-                                            className="flex items-center justify-between mb-2 border-b border-gray-600 cursor-pointer"
-                                            onClick={() => handleToggleCollapse(tagType)}
-                                        >
-                                            <span
-                                                className="mt-0 p-0"
-                                                style={{ textShadow: `2px 2px 4px ${tags[0].tag_type.color}`, color: 'white' }}
-                                            >
-                                                {tagType}
-                                            </span>
-                                            <span>{collapsedTags[tagType] ? '▼' : '▲'}</span>
-                                        </div>
-                                        {!collapsedTags[tagType] && (
-                                            <div className="flex flex-wrap gap-2">
-                                                {tags.map((tag) => (
-                                                    <div key={tag.id} className="flex items-center">
-                                                        <input
-                                                            type="checkbox"
-                                                            id={tag.name}
-                                                            value={tag.name}
-                                                            checked={selectedTags.some((t) => t.id === tag.id)}
-                                                            onChange={() => handleTagSelection(tag)}
-                                                            className="mr-2"
-                                                        />
-                                                        <label
-                                                            htmlFor={tag.name}
-                                                            className="px-2 py-1 rounded"
-                                                            style={{ backgroundColor: tag.tag_type.color, color: 'white' }}
-                                                        >
-                                                            {tag.name}
-                                                        </label>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        )}
-                                    </div>
-                                ))}
-                            </div>
+                                    )}
+                                </div>
+                            ))}
                         </div>
                         <button
-                            ref={closeButtonRef}
+                            ref={filterCloseButtonRef}
                             onClick={closeFilterModal}
-                            className="modal-close-button text-white bg-blue-500 px-4 py-2 rounded mt-4 absolute left-1/2 transform -translate-x-1/2"
+                            className="modal-close-button"
                         >
                             Close
                         </button>
@@ -303,60 +290,62 @@ const CalendarView: React.FC = () => {
 
             {isModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 overflow-y-auto">
-                    <div ref={modalRef} className="modal-container bg-gray-300 text-black p-6 rounded-lg shadow-lg">
-                        <h2 className="text-2xl font-bold mb-4 text-center">
+                    <div ref={modalRef} className="flex-modal-container bg-gray-300 text-black rounded-lg shadow-lg">
+                        <h2 className="text-2xl font-bold mb-0 text-center">
                             {getFormattedDate(selectedDate)}
                         </h2>
-                        {selectedDatePosts.length > 0 ? (
-                            <ul className="space-y-4">
-                                {selectedDatePosts.map(post => (
-                                    <li key={post.id} className="mb-4 p-4 border rounded bg-gray-800 text-white">
-                                        <p className="text-sm text-gray-500">{format(new Date(post.created_at), "MMMM do, yyyy 'at' h:mm a")}</p>
-                                        <p className="text-2xl font-bold text-stone-400 ">{post.title}</p>
+                        <div className="modal-content">
+                            {selectedDatePosts.length > 0 ? (
+                                <ul className="space-y-4">
+                                    {selectedDatePosts.map(post => (
+                                        <li key={post.id} className="mb-4 p-4 border rounded bg-gray-800 text-white">
+                                            <p className="text-sm text-gray-500">{format(new Date(post.created_at), "MMMM do, yyyy 'at' h:mm a")}</p>
+                                            <p className="text-2xl font-bold text-stone-400 ">{post.title}</p>
 
-                                        <div className="text-white">
-                                            {truncateHtmlContent(post.content, 100)}
-                                        </div>
-                                        {post.tags && post.tags.length > 0 && (
-                                            <div className="mt-2">
-                                                <ul className="flex flex-wrap gap-2 mt-1">
-                                                    {post.tags.map(tag => (
-                                                        <li key={tag.id} className="px-2 py-1 rounded" style={{
-                                                            backgroundColor: tag.tag_type.color,
-                                                            color: 'white'
-                                                        }}>
-                                                            {tag.name}
-                                                        </li>
-                                                    ))}
-                                                </ul>
+                                            <div className="text-white">
+                                                {truncateHtmlContent(post.content, 100)}
                                             </div>
-                                        )}
-                                        <div style={{ marginTop: '10px' }} className="flex flex-wrap gap-2">
-                                            <Link href={`/posts/${post.id}`} className="text-blue-300 hover:underline">Read
-                                                more</Link>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        ) : (
-                            <div className="flex flex-col justify-center">
-                                <p className="text-center mb-4">No Journal Entries for this Date.</p>
-                                <Link href={`/posts/new?date=${selectedDate?.toISOString()}`}
-                                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
-                                      style={{ width: "323px", margin: "auto" }}
-                                >
-                                    + Create New Journal Entry
-                                </Link>
-                            </div>
-                        )}
+                                            {post.tags && post.tags.length > 0 && (
+                                                <div className="mt-2">
+                                                    <ul className="flex flex-wrap gap-2 mt-1">
+                                                        {post.tags.map(tag => (
+                                                            <li key={tag.id} className="px-2 py-1 rounded" style={{
+                                                                backgroundColor: tag.tag_type.color,
+                                                                color: 'white'
+                                                            }}>
+                                                                {tag.name}
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            )}
+                                            <div style={{ marginTop: '10px' }} className="flex flex-wrap gap-2">
+                                                <Link href={`/posts/${post.id}`} className="text-blue-300 hover:underline">Read
+                                                    more</Link>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <div className="flex flex-col justify-center">
+                                    <p className="text-center mb-4">No Journal Entries for this Date.</p>
+                                    <Link href={`/posts/new?date=${selectedDate?.toISOString()}`}
+                                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
+                                          style={{ width: "323px", margin: "auto" }}
+                                    >
+                                        + Create New Journal Entry
+                                    </Link>
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            ref={closeButtonRef}
+                            onClick={closeModal}
+                            className="modal-close-button"
+                        >
+                            Close
+                        </button>
                     </div>
-                    <button
-                        ref={closeButtonRef}
-                        onClick={closeModal}
-                        className="modal-close-button text-white bg-blue-500 px-4 py-2 rounded mt-4"
-                    >
-                        Close
-                    </button>
                 </div>
             )}
         </div>
